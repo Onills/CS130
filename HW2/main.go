c91a9a8c8288d2c3d831083cf239029702e326f0
Carter Kimbo <Carter Kimbo>
2016-02-03 19:10:07 -0800

    Second Commit

---
+++ HW2/main.go
@@ -0,0 +1,14 @@
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 99
+	y := "IQ"
+	z := 9.81
+
+	fmt.Printf("A : (25) is a varible type : %T.\n", x)
+	fmt.Printf("B : (EQ) is a varible type : %T.\n", y)
+	fmt.Printf("C : (9.81) is a varible type : %T.\n", z)
+}
